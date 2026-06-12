7230766af415dad4f940c191c5a98f597ca7ea99
VishSeran <vishwa.sovis@gmail.com>
2026-06-12 11:58:38 +0530

    define embedding model updated

--- src/utilities.py
+++ src/utilities.py
@@ -1,14 +1,25 @@
 from langchain_ollama.llms import OllamaLLM
-
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
 #define llm model
 def llm_model (model_id):
     model_name = "llama3.1"
     
     llm = OllamaLLM(
-        model=model_name
+        model=model_name,
+        temperature=0.5,
     )
     
     return llm
+
+#define embedding model
+def embeddings():
+    
+    embed_model = HuggingFaceEmbeddings(
+        model = "BAAI/bge-m3"
+    )
+    
+    return embed_model
 
 
     
